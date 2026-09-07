2d8d48c9f8e1dcabd7c6c7b1efae0126c01b1227
Akhil Krishnan <57069212+aukay13@users.noreply.github.com>
2026-09-07 13:08:59 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[]dp = new int[n+1];
+        return func(n, dp);
+    }
+
+    public int func(int n, int[]dp){
+        if(n==1 || n==2){
+            return n;
+        }
+
+        if(dp[n]!=0){
+            return dp[n];
+        }
+        dp[n] = func(n-1, dp) + func(n-2, dp);
+
+        return dp[n];
+    }
+}
